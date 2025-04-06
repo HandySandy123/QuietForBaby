4c94262f9ea03bb52bfdacab98cdbc4889aa4116
HandySandy123 <151861458+HandySandy123@users.noreply.github.com>
2025-04-06 08:59:54 +0200

    player fixes

--- Quiet For Mommy/Assets/Scripts/PlayerBehavior.cs
+++ Quiet For Mommy/Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine.Serialization;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    SpriteRenderer spr; 
+    SpriteRenderer spr;
     Rigidbody rb;
 
     private Vector2 Movement;
@@ -12,26 +12,33 @@ public class PlayerBehavior : MonoBehaviour
     [FormerlySerializedAs("MoveSpeed")] [SerializeField] private float moveSpeed = 10f;
 
     private PlayerInput _playerInput;
+    [SerializeField] private Sprite[] sprites;
     
-    public Vector3 camPos;
+    //public Vector3 camPos;
 
-    [SerializeField] private float distToCam;
+    //[SerializeField] private float distToCam = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
         _playerInput = GetComponent<PlayerInput>();
         groundDist = spr.bounds.extents.x;
     }
-    
+
+    void Awake()
+    {
+        Ray r = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+        //camPos = r.GetPoint(distToCam);
+    }
+
     // Update is called once per frame
     void Update()
     {
         Movement = _playerInput.actions["OnMovement"].ReadValue<Vector2>();
-        
-        
-        //Debug.Log(Movement);
+
+        // Raycast to adjust Y position based on ground distance
         RaycastHit hit;
         Vector3 castPos = transform.position;
         castPos.y += 1;
@@ -44,15 +51,58 @@ public class PlayerBehavior : MonoBehaviour
                 transform.position = movePos;
             }
         }
-        
-        Ray r = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
-        camPos = r.GetPoint(distToCam);
-        
     }
 
     void FixedUpdate()
     {
-        //rb.linearVelocity = new Vector3(Movement.x * moveSpeed, 0, Movement.y * moveSpeed);
-        rb.linearVelocity = (new Vector3(Movement.x * moveSpeed, 0, 0) + new Vector3(0, 0, Movement.y * moveSpeed)) / 2;
+        // Apply movement based on input using rb.velocity
+        Vector3 movementDirection = new Vector3(Movement.x, 0, Movement.y) * moveSpeed;
+        rb.linearVelocity = movementDirection;
+        //rb.AddForce(movementDirection * moveSpeed, ForceMode.Impulse);
+        Debug.Log(rb.GetAccumulatedForce());
+
+        // Debugging movement
+        //Debug.Log(rb.linearVelocity);
+
+        // if (Movement.x != 0 || Movement.y != 0)
+        // {
+        //     controlAnimation(Movement);
+        // }
+        // else
+        // {
+        //     spr.sprite = sprites[3]; // Idle sprite
+        // }
+    }
+
+    void controlAnimation(Vector2 input)
+    {
+        float y = input.y;
+        float x = input.x;
+        
+        // Conditions for each direction
+        bool upWalk = (x == 0 && Mathf.Approximately(y, 1)) || // Up
+                       Mathf.Approximately(x, 0.71f) && Mathf.Approximately(y, 0.71f) || // Up-right
+                       Mathf.Approximately(x, 1) && y == 0; // Right
+        bool rightWalk = !Mathf.Approximately(x, 1) && !Mathf.Approximately(x, -1) && Mathf.Approximately(y, 0.71f);
+        bool RDwalk = (x == 0 && Mathf.Approximately(y, -1)) ||
+                       y == 0 && Mathf.Approximately(x, -1); // Down-left
+
+        bool downWalk = !upWalk || !rightWalk || !RDwalk;
+
+        // Assign correct sprite based on movement direction
+        if (upWalk)
+        {
+            spr.sprite = sprites[0]; // Up
+        }
+        else if (rightWalk)
+        {
+            spr.sprite = sprites[1]; // Right
+            spr.flipX = (Mathf.Approximately(x, 0.71f) && Mathf.Approximately(y, 0.71f)) ? true : false;
+        }
+        else if (RDwalk)
+        {
+            spr.sprite = sprites[2]; // Down-left
+            spr.flipX = (Mathf.Approximately(x, -1) && y == 0) ? true : false;
+        }
     }
 }
